df0e4adfe5d52adb95704cb8ea3a146ca01d3361
Lester Corpuz <lester19.corpuz28@gmail.com>
2025-08-04 16:12:19 +0800

    added auth-ldap files

---
+++ 03-authenticating-ldap/src/test/java/com/learning/authenticating_ldap/AuthenticatingLdapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.authenticating_ldap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticatingLdapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
